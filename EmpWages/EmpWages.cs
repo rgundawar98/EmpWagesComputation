02793debfdf8f2a67fed8e0b168b2925cc099a10
rahulgundawar98 <rahulgundawar98@gmail.com>
2023-02-09 18:10:49 +0530

    Adding parttime and calculating wages UC3_AddPartTimeandWage

--- EmpWages/EmpWages.cs
+++ EmpWages/EmpWages.cs
@@ -10,21 +10,26 @@ namespace EmpWages
     {
         public static void Employee()
         {
-            int Is_Present = 1;
+            int Is_FullTime = 1;
+            int Is_PartTime = 2;
             int Emp_Rate_Per_hour = 20;
             int empHr = 0;
             int empwage = 0;
             Random random = new Random(); 
-            int empCheck = random.Next(2);
-            if(empCheck == Is_Present)
+            int empCheck = random.Next(3);
+            if(empCheck == Is_FullTime)
             {
-                Console.WriteLine("Employee is present");
+                Console.WriteLine("Employee is Full time");
                 empHr = 8;
+            }
+            else if(empCheck == Is_PartTime)
+            {
+                Console.WriteLine("Employee is Part Time");
+                empHr = 4;
             }
             else
             {
                 Console.WriteLine("Employee is absent");
-                empHr = 0;
             }
             empwage = empHr * Emp_Rate_Per_hour;
             Console.WriteLine("Daily wages will be :" + empwage);
