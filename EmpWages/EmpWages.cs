2339ca3daf2d66f90c70c477ee1940bb5b1807d4
rahulgundawar98 <rahulgundawar98@gmail.com>
2023-02-09 18:04:11 +0530

    Checking daily wages of employee UC2_DailyWages

--- EmpWages/EmpWages.cs
+++ EmpWages/EmpWages.cs
@@ -11,16 +11,23 @@ namespace EmpWages
         public static void Employee()
         {
             int Is_Present = 1;
+            int Emp_Rate_Per_hour = 20;
+            int empHr = 0;
+            int empwage = 0;
             Random random = new Random(); 
             int empCheck = random.Next(2);
             if(empCheck == Is_Present)
             {
                 Console.WriteLine("Employee is present");
+                empHr = 8;
             }
             else
             {
                 Console.WriteLine("Employee is absent");
+                empHr = 0;
             }
+            empwage = empHr * Emp_Rate_Per_hour;
+            Console.WriteLine("Daily wages will be :" + empwage);
         }
     }
 }
