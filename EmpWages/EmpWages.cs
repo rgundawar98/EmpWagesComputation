56f870d7af017077fd92f7dc8aa26141a3ae7ba7
rahulgundawar98 <rahulgundawar98@gmail.com>
2023-02-10 13:21:48 +0530

    Checking total emaployee wages UC5_CheckTotalwage

--- EmpWages/EmpWages.cs
+++ EmpWages/EmpWages.cs
@@ -11,28 +11,36 @@ namespace EmpWages
         const int Is_FullTime = 1;
         const int Is_PartTime = 2;
         const int Emp_Rate_Per_hour = 20;
+        const int Number_Of_Working_Days = 20;
         public static void Employee()
         {
             int empHr = 0;
             int empwage = 0;
-            Random random = new Random(); 
-            int empCheck = random.Next(3);
-            switch(empCheck)
+            int TotalEmpWage = 0;
+
+            for (int day = 0; day < Number_Of_Working_Days; day++)
             {
-                case Is_FullTime:
-                   Console.WriteLine("Employee is Full time");
-                   empHr = 8;
-                   break;
-                case Is_PartTime:
-                    Console.WriteLine("Employee is Part Time");
-                    empHr = 4;
-                    break;
-                default:
-                    Console.WriteLine("Employee is absent");
-                    break;
+                Random random = new Random();
+                int empCheck = random.Next(3);
+                switch (empCheck)
+                {
+                    case Is_FullTime:
+                        Console.WriteLine("Employee is Full time");
+                        empHr = 8;
+                        break;
+                    case Is_PartTime:
+                        Console.WriteLine("Employee is Part Time");
+                        empHr = 4;
+                        break;
+                    default:
+                        Console.WriteLine("Employee is absent");
+                        break;
+                }
+                empwage = empHr * Emp_Rate_Per_hour;
+                TotalEmpWage+= empwage;
+                Console.WriteLine("Emp wage:"+empwage);
             }
-            empwage = empHr * Emp_Rate_Per_hour;
-            Console.WriteLine("Daily wages will be :" + empwage);
+               Console.WriteLine("Total employee wage will be :" +TotalEmpWage);
         }
     }
 }
